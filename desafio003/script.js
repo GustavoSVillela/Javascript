242260ac5b55cee9cc9a55918582e88cd6a27ae6
GustavoSVillela <villelagustavo33@gmail.com>
2025-04-28 10:43:56 -0300

    create super

---
+++ desafio003/script.js
@@ -0,0 +1,25 @@
+function contar(){
+    var n1 = document.getElementById ('n1')
+    var n2 = document.getElementById('n2')
+    var n3 = document.getElementById('n3')
+    var res = document.getElementById('res')
+    if (n2.value == 0 || n3.value == 0) {
+        window.alert('[ERRO] Verifique os dados novamente!')
+
+    } else if (n1.value < n2.value) {
+        var fim = Number (n2.value)
+        var pass = Number (n3.value)
+
+        for (var ini = Number (n1.value); ini <= fim; ini+=pass)
+            res.innerHTML += `${ini} 👉.. `
+    } else {
+            var fim = Number (n2.value)
+            var pass = Number (n3.value)
+            for (var ini = Number (n1.value); ini >= fim; ini -= pass)
+                res.innerHTML += `${ini} 👉..  `
+        }
+
+    
+        
+    
+}    
