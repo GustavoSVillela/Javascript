ae8cd61d0fac205753437141a0567114e32c0a84
GustavoSVillela <villelagustavo33@gmail.com>
2025-04-28 19:03:28 -0300

    update

--- desafio004/script.js
+++ desafio004/script.js
@@ -1,16 +1,18 @@
-function gerar(){
-    var tab = document.getElementById ('tab')
-    var res = document.getElementById ('res')
-    var n1
-    var n2
-    if (tab.value == 0 ){
-        window.alert ('[ERRO] Verifique novamente as informações')
-    } else {
-        var num = Number (tab.value)
-        res.innerHTML = ''
-        for (var i = 1; i <= 10; i++){
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.lenght == 0){
 
-        res.innerHTML += `${num} x ${i} = ${num * i} <br/>`
+        alert('Por favor, digite um número!')
+    } else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c<=10){
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
         }
     }
 }
