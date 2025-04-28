cb273c0a7a1987fc0c3d7821a0dafc8c7963b51d
GustavoSVillela <villelagustavo33@gmail.com>
2025-04-28 13:17:31 -0300

    update

--- desafio004/script.js
+++ desafio004/script.js
@@ -1,4 +1,19 @@
 function gerar(){
-    var tab = document.getElementById ("tab")
-    
+    var tab = document.getElementById ('tab')
+    var res = document.getElementById ('res')
+    var n1
+    var n2
+    if (tab.value == 0 ){
+        window.alert ('[ERRO] Verifique novamente as informações')
+    } else {
+        var num = Number (tab.value)
+        res.innerHTML = ''
+        for (var i = 1; i <= 10; i++){
+
+        res.innerHTML += `${num} x ${i} = ${num * i} <br/>`
+        }
+    }
 }
+    
+        
+
