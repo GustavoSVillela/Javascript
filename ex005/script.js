2fc7050bdde57f9b3ae964a904857f6b6f61a811
GustavoSVillela <villelagustavo33@gmail.com>
2025-04-29 21:20:54 -0300

    create arrays

---
+++ ex005/script.js
@@ -0,0 +1,10 @@
+let valores = [1,7,4,2,9]
+
+
+
+for(let pos in valores){
+    console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
+
+}
+pos = valores.indexOf(7)
+console.log(`os valores esta no posicao ${pos}`)
